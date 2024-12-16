2d384977fc43099a0debf3378a37dedbd7cc64cf
RaviniKuruppu <ravinikuruppu@gmail.com>
2024-12-16 22:34:38 +0530

    changes in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,18 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <nav>
+      <nav className='nav' >
         <ul>
           <li><a href="#about">About</a></li>
+          <li><a href="#projects">Experience</a></li>
           <li><a href="#projects">Projects</a></li>
+          <li><a href="#projects">Technologies</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
+      <br></br>
       <h1>RAVINI KURUPPU</h1>
       <h2>Full Stack Developer</h2>
-      <h2>Computer Science and Engineering</h2>
-      <h3>University of Moratuwa</h3>
     </header>
   );
 }
