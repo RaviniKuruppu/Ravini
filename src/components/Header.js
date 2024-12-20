8ee10da2e7368a716de2e0b17f87cfe13a1c68dc
RaviniKuruppu <ravinikuruppu@gmail.com>
2024-12-20 08:43:47 +0530

    add icons to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Header.css';
-import RaviniImage from '../images/Ravini2.png';
+import RaviniImage from '../images/Ravini.png';
+import { FaLinkedin, FaGithub, FaMedium } from 'react-icons/fa';
+
 
 
 function Header() {
@@ -23,6 +25,17 @@ function Header() {
           <h1>RAVINI KURUPPU</h1>
           <h2>Computer Science and Engineering Undergraduate</h2>
           <button class="button-90" >Download CV</button>
+          <div className="social-icons">
+            <a href="https://www.linkedin.com/in/ravini-kuruppu/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin />
+            </a>
+            <a href="https://github.com/RaviniKuruppu" target="_blank" rel="noopener noreferrer">
+              <FaGithub />
+            </a>
+            <a href="https://medium.com/@ravinikuruppu" target="_blank" rel="noopener noreferrer">
+              <FaMedium />
+            </a>
+          </div>
         </div> 
       </div>
       
