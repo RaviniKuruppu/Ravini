55fecee857cd5e4c851f2f835209f94ccbbd3c38
RaviniKuruppu <ravinikuruppu@gmail.com>
2024-12-24 10:42:00 +0530

    changes in the experience section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Experience.css';
+import moratuwa_logo from '../images/UOM.PNG';
+import BoardPac_logo from '../images/BoardPac.PNG';
 
 function Experience() {
     return (
@@ -8,13 +10,13 @@ function Experience() {
             <div className="timeline">
                 <div className="timeline-item">
                     <div className="timeline-icon">
-                        <img src="moratuwa_logo.png" alt="Moratuwa Logo" />
+                        <img src={moratuwa_logo} alt="Moratuwa Logo" />
                     </div>
                     <div className="timeline-content">
                         <h3>BSc Engineering - University of Moratuwa (CSE Department)</h3>
                         <p>
                             B.Sc Engineering with specialization in Computer Science and Engineering<br />
-                            GPA: 3.77/4.00<br />
+                            GPA: 3.4/4.00<br />
                             Earned placement in the Dean's List thrice<br />
                             Involved in maintenance of the CSE Department website
                         </p>
@@ -23,10 +25,10 @@ function Experience() {
 
                 <div className="timeline-item">
                     <div className="timeline-icon">
-                        <img src="wso2_logo.png" alt="WSO2 Logo" />
+                        <img src={BoardPac_logo} alt="WSO2 Logo" />
                     </div>
                     <div className="timeline-content">
-                        <h3>Intern - Software Engineer -Ironone</h3>
+                        <h3>Intern - Software Engineer -BoardPAC</h3>
                         <p>
                             Member of the Identity and Access Management team.<br />
                             Developed JavaScript SDKs to integrate authentication.<br />
