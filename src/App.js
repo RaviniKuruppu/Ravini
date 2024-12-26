a5d5777746bb6b124d707b614a57c9ae921e11d6
RaviniKuruppu <ravinikuruppu@gmail.com>
2024-12-26 21:51:16 +0530

    changes in contacts

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Experience from './components/Experience';
 import Technologies from './components/Technologies';
-import backgroundImage from './images/black-and-white-web-design-ru.jpg'
+import backgroundImage from './images/black-and-white-web-design-ru.jpg';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
   return (
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,29 @@ import './Contact.css';
 function Contact() {
   return (
     <section id="contact" className="contact">
-      <h2>Contact</h2>
-      <p>Feel free to reach out to me via email: <a href="mailto:your.email@example.com">your.email@example.com</a></p>
+      <h2>Contact Me</h2>
+            <div className="contact-details">
+                <div className="contact-item">
+                    <i className="fas fa-envelope"></i>
+                    <span>ivandan@email.com</span>
+                </div>
+                <div className="contact-item">
+                    <i className="fas fa-phone-alt"></i>
+                    <span>(201) 555-0124</span>
+                </div>
+                <div className="contact-item">
+                    <i className="fas fa-map-marker-alt"></i>
+                    <span>Toledo, United States</span>
+                </div>
+            </div>
+            <div className="social-icons">
+                <i className="fab fa-facebook"></i>
+                <i className="fab fa-twitter"></i>
+                <i className="fab fa-linkedin"></i>
+                <i className="fab fa-whatsapp"></i>
+            </div>
+            <p className="footer-text">“Thanks for Scrolling”</p>
+
     </section>
   );
 }
