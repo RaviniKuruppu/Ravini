5d995bec91447f79c64a0a3e39f98c445d6beb72
RaviniKuruppu <ravinikuruppu@gmail.com>
2025-01-03 14:43:59 +0530

    add project content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Projects.css';
-import busArrivalImg from '../images/BoardPac.PNG'; // Replace with actual image paths
-import rpalInterpreterImg from '../images/BoardPac.PNG';
-import bankTransactionImg from '../images/BoardPac.PNG';
+import Orphansafe_logo from "../images/Projects/ophansafe.png";
+import StudentMS_logo from "../images/Projects/student-management-system.png";
+import HumanMS_logo from "../images/Projects/humanResourceMS.jpg";
+import EventMS_logo from "../images/Projects/EventMS.jfif";
+import logo_reg_logo from "../images/Projects/Untitled design (2).png";
 
 function Projects() {
   return (
@@ -11,62 +13,68 @@ function Projects() {
       <div className="project-list">
         {/* Project 1 */}
         <div className="project-card">
-          <img src={busArrivalImg} alt="Bus Arrival Time Prediction" className="project-image" />
+          <img src={logo_reg_logo} alt="logo_reg_logo" className="project-image" />
           <div className="project-content">
             <h3>Brand Visual Registration and Lookup System | Final year project </h3>
             <p>Web Application| Machine learning | Image recognition| Vector graphics</p>
             <p><strong>Technologies:</strong> Python, Milvus Vector Database, React.js</p>
             <p>Develop a brand visual registration system with an API for accurate lookup, capable of distinguishing subtle differences between similar visuals, supported by an end-to-end SVG pipeline.</p>
+            <button class="custom-button">More Details</button>
           </div>
         </div>
 
         {/* Project 2 */}
         <div className="project-card">
-          <img src={rpalInterpreterImg} alt="RPAL Interpreter" className="project-image" />
+          <img src={Orphansafe_logo} alt="Orphansafe_logo" className="project-image" />
           <div className="project-content">
             <h3>Data privacy MIS for child protection authority</h3>
             <p>Web and mobile application | CI/CD | Microservice | Cloud technology</p>
             <p><strong>Technologies:</strong> React JS, NodeJS, Express, Postgres SQL, AWS Blob, RabbitMQ, Docker, Flutter</p>
             <p>This is an application developed for child protection authority to protect children's data while allowing authorized persons to access and handle it.</p>
             <p><strong>Contribution:</strong> Frontend service, Profile management service</p>
+            <button class="custom-button">More Details</button>
           </div>
         </div>
 
         {/* Project 3 */}
         <div className="project-card">
-          <img src={bankTransactionImg} alt="Bank Transaction Management System" className="project-image" />
+          <img src={EventMS_logo} alt="Event Management System" className="project-image" />
           <div className="project-content">
             <h3>Event Management System| Individual Project</h3>
             <p>Web and mobile application | Three-tier architecture</p>
             <p><strong>Technologies:</strong> Flutter, Javascript, NodeJS, Express, MySQL</p>
             <p>The Event Management System is a centralized platform that improves event organization, scheduling, communication, registration, and monitoring with enhanced user engagement and real-time notifications.</p>
+            <button class="custom-button">More Details</button>
           </div>
         </div>
-      </div>
+      
 
       {/* Project 4 */}
         <div className="project-card">
-          <img src={rpalInterpreterImg} alt="RPAL Interpreter" className="project-image" />
+          <img src={StudentMS_logo} alt="StudentMS" className="project-image" />
           <div className="project-content">
             <h3>Student Management System| Individual Project</h3>
             <p>Web application | Clean Architecture| SOLID Principles | EF Core Code-First Approach| Design Patterns</p>
             <p><strong>Technologies:</strong> Angular, Asp .Net core, PostgreSQL</p>
             <p>This student management application, built with Angular for the user interface and ASP.NET Core for server-side logic, is designed to explore tech stacks, clean architecture, and design patterns.</p>
+            <button class="custom-button">More Details</button>
           </div>
         </div>
       
       {/* Project 5 */}
       <div className="project-card">
-          <img src={rpalInterpreterImg} alt="RPAL Interpreter" className="project-image" />
+          <img src={HumanMS_logo} alt="HumanMS_logo" className="project-image" />
           <div className="project-content">
             <h3>Human Resource Management System| Group Project</h3>
             <p>Web application</p>
             <p><strong>Technologies:</strong> HTML, CSS, Javascript, NodeJS, Express, MySQL </p>
             <p>A web-based human resource management system that includes a personal information management module, a leave management module, and a reporting module.</p>
             <p><strong>Contribution:</strong> Front-end development, Database</p>
+            <button class="custom-button">More Details</button>
           </div>
         </div>
 
+        </div>
     </section>
   );
 }
